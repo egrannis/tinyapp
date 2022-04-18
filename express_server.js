312dfab83792efce29a429e6b7a02ae5109c935c
Emma Grannis <grannis.emma@gmail.com>
2022-04-18 21:37:36 +0000

    added a new route handler for /urls and used res.render() to pass data to template

--- express_server.js
+++ express_server.js
@@ -4,9 +4,14 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+"b2xVn2": "http://www.lighthouselabs.ca",
+"9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (req, res) => {
+const templateVars = {urls: urlDatabase};
+res.render("urls_index", templateVars); // since we're using the Express convention of using a views directory, we don't have to tell express where to find the file
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
