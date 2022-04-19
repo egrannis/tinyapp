cb74e18bdcd1502c69826a43979fa198b79ac678
Emma Grannis <grannis.emma@gmail.com>
2022-04-19 00:47:38 +0000

    fixed a typo on line 16

--- express_server.js
+++ express_server.js
@@ -13,8 +13,8 @@ app.get("/urls", (req, res) => { // define our route, which is /urls
   res.render("urls_index", templateVars); // since we're using the Express convention of using a views directory, we don't have to tell express where to find the file
 });
 
-app.get("urls/new", (req, res) => {
-  res.render("urls_new")
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
