bd6c669f430b2fd28667de4ba18687c3dfd98319
Emma Grannis <grannis.emma@gmail.com>
2022-04-19 01:37:42 +0000

    Fixed mistkae on line 23/24 with my shortURl and longURL key value pair

--- express_server.js
+++ express_server.js
@@ -20,8 +20,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const tempVars = { shortURL: req.params.shortURL, longURL: req.params.longURL
-  }
+  const tempVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", tempVars);
 });
 
@@ -40,6 +39,7 @@ app.get("/hello", (req, res) => {
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+
 });
 
 function generateRandomString() {
