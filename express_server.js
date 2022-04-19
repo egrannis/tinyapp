d7ede4d8cb3699091d9dc804a64cf0f0093df211
Emma Grannis <grannis.emma@gmail.com>
2022-04-19 00:51:48 +0000

    Added bodyparser lines of code on lines 5 and 6

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
 "b2xVn2": "http://www.lighthouselabs.ca",
