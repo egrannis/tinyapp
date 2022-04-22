c3c3adda4f12cda66c55f6bd0047a8acb9080a23
Emma Grannis <grannis.emma@gmail.com>
2022-04-22 19:11:58 +0000

    Moved helper functions into different file

---
+++ helpers.js
@@ -0,0 +1,33 @@
+
+
+const findUserByEmail = (email, database) => {
+  const givenEmail = email;
+  for (let userKey in database) {
+    if (database[userKey].email === givenEmail) {
+      return database[userKey];
+    }
+  }
+  return false;
+};
+
+const urlsForUser = (id, database) => {
+  const userURLS = {};
+  for (let shortURL in database) {
+    if (database[shortURL].userID === id) {
+      userURLS[shortURL] = database[shortURL];
+    }
+  }
+  return userURLS;
+};
+
+const generateRandomString = () => {
+  let string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randString = '';
+ 
+  for (let i = 0; i < 6; i++) {
+    randString += string[Math.floor(Math.random() * string.length)];
+  }
+  return randString;
+};
+
+module.exports = {findUserByEmail, urlsForUser, generateRandomString};
